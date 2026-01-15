fa52a9bbd4842823d0257e6623582a9ce5982b68
宋文武 <iyzsong@envs.net>
2026-01-15 19:09:35 +0800

    匹配原始文件

---
+++ x.asm
@@ -0,0 +1,7 @@
+        ;; XXX: 官方下载的文件有这些数据，感觉没用到啊。
+bank_table:     .section
+        .bank_table
+        .bankaddr $05874b, $056646, $057c09, $057baa
+        .bankaddr $0156d6, $0156da, $0156db, $0156dc
+        .bankaddr $0156dd, $0156de, $0156ed, $0156ee
+        .bankaddr $0156ef, $0156f0
